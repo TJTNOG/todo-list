dd075fd8f361b13a2a11c79b656850ee5625eae5
Jin <jttang29@gmail.com>
2023-05-13 09:10:38 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.use(bodyParser.urlencoded({ extended: true}))
 app.get("/", (req, res) => {
   Todo.find() // 取出 Todo所有資料
     .lean() // 把Mongoose 的 Model物件轉換成乾淨的JS資料陣列
+    .sort({ _id: 'asc'}) //desc
     .then((todos) => res.render("index", { todos })) // 將資料傳給 index 樣板
     .catch((error) => console.error(error));
 });
