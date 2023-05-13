6d3b5a4510a7f716dd948aa98496fe724405d492
Jin <jttang29@gmail.com>
2023-05-13 13:28:19 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -1,97 +1,45 @@
 // 載入 express 並建構應用程式伺服器
 const express = require("express");
 const mongoose = require("mongoose"); // 載入mongoose
-const exphbs = require('express-handlebars')
+const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
-const methodOverride = require("method-override") //載入method-override
-const Todo = require('./models/todo')
+const methodOverride = require("method-override"); //載入method-override
+const Todo = require("./models/todo");
 
+const routes = require("./routes");
 const app = express();
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
 // 設定連線到 mongoDB
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}); 
+});
 
 // 取得資料庫連線狀態
-const db = mongoose.connection
+const db = mongoose.connection;
 // 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
+db.on("error", () => {
+  console.log("mongodb error!");
+});
 // 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
-
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
-app.set('view engine', 'hbs')
-
-app.use(bodyParser.urlencoded({ extended: true}))
-
-app.use(methodOverride('_method'))
-// 設定首頁路由
-app.get("/", (req, res) => {
-  Todo.find() // 取出 Todo所有資料
-    .lean() // 把Mongoose 的 Model物件轉換成乾淨的JS資料陣列
-    .sort({ _id: 'asc'}) //desc
-    .then((todos) => res.render("index", { todos })) // 將資料傳給 index 樣板
-    .catch((error) => console.error(error));
+db.once("open", () => {
+  console.log("mongodb connected!");
 });
 
-app.get("/todos/new", (req, res) => {
-  return res.render("new");
-});
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
 
-app.post("/todos", (req, res) => {
-  const name = req.body.name; // 從 req.body 拿出表單裡的 name 資料
-  return Todo.create({ name }) // 存入資料庫
-    .then(() => res.redirect("/")) // 新增完成後導回首頁
-    .catch((error) => console.log(error));
-});
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/todos/:id", (req, res) => {
-  const id = req.params.id;
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => res.render("detail", { todo }))
-    .catch((error) => console.log(error));
-});
+app.use(methodOverride("_method"));
 
-app.get("/todos/:id/edit", (req, res) => {
-  const id = req.params.id;
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => res.render("edit", { todo }))
-    .catch((error) => console.log(error));
-});
+app.use(routes);
 
-app.put("/todos/:id", (req, res) => {
-  const id = req.params.id;
-  const {name, isDone} = req.body
-  return Todo.findById(id)
-    .then((todo) => {
-      todo.name = name;
-      todo.isDone = isDone === 'on'
-      return todo.save();
-    })
-    .then(() => res.redirect(`/todos/${id}`))
-    .catch((error) => console.log(error));
-});
-
-app.delete('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .then(todo => todo.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
 // 設定port 3000
 app.listen(3000, () => {
   console.log(`App is running in http://localhost:3000`);
